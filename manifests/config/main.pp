140ceba756da7c48ab425505b568bace922f378a
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2018-02-21 14:48:47 +0100

    add local_parameters parameter for main configs

--- manifests/config/main.pp
+++ manifests/config/main.pp
@@ -21,20 +21,27 @@
 #  $parameters
 #    The parameters to set in the main.cf file
 #    Defaults to {}
+#  $local_parameters
+#    another posibility to set parameters.
+#    thought for local changes (eg on node level)
+#    Defaults to {}
 #
 class postfix::config::main (
-  $main_cf_file = $postfix::params::main_cf_file,
-  $owner        = $postfix::params::owner,
-  $group        = $postfix::params::group,
-  $mode         = $postfix::params::mode,
-  $parameters   = {},
+  $main_cf_file     = $postfix::params::main_cf_file,
+  $owner            = $postfix::params::owner,
+  $group            = $postfix::params::group,
+  $mode             = $postfix::params::mode,
+  $parameters       = {},
+  $local_parameters = {},
 ) inherits ::postfix::params {
+
+  $_parameters=merge($parameters, $local_parameters)
 
   file { $main_cf_file :
     owner   => $owner,
     group   => $group,
     mode    => $mode,
-    content => epp('postfix/main.cf.epp',{ parameters => $parameters } ),
+    content => epp('postfix/main.cf.epp',{ parameters => $_parameters } ),
     notify  => Service['postfix'],
   }
 
